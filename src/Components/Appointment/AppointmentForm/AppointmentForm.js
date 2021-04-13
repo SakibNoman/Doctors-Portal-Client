2554c4fe5d425b9313fd00424db3c083a5fd70db
Sakib Noman <skbnmn@gmail.com>
2021-04-14 00:14:01 +0600

    modal

--- src/Components/Appointment/AppointmentForm/AppointmentForm.js
+++ src/Components/Appointment/AppointmentForm/AppointmentForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 import Modal from 'react-modal';
 
 const customStyles = {
@@ -14,9 +15,33 @@ const customStyles = {
 
 Modal.setAppElement('#root')
 
-const AppointmentForm = () => {
+const AppointmentForm = ({ modalIsOpen, closeModal, appointmentOn }) => {
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+
     return (
         <div>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+            >
+
+                <h2 className="text-fresh text-center" >{appointmentOn}</h2>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    {/* register your input into the hook by invoking the "register" function */}
+                    <input defaultValue="test" {...register("example")} />
+
+                    {/* include validation with required or other standard HTML validation rules */}
+                    <input {...register("exampleRequired", { required: true })} />
+                    {/* errors will return when field validation fails  */}
+                    {errors.exampleRequired && <span>This field is required</span>}
+
+                    <input type="submit" />
+                </form>
+            </Modal>
 
         </div>
     );
--- src/Components/Appointment/BookingCard/BookingCard.js
+++ src/Components/Appointment/BookingCard/BookingCard.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AppointmentForm from '../AppointmentForm/AppointmentForm';
 
 const BookingCard = ({ booking }) => {
+    const [modalIsOpen, setIsOpen] = useState(false);
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
     return (
         <div className="col-md-4 mb-5">
             <div className="card p-3">
@@ -8,7 +17,8 @@ const BookingCard = ({ booking }) => {
                     <h5 className="card-title text-fresh">{booking.subject}</h5>
                     <h6>{booking.visitingHour}</h6>
                     <p>{booking.totalSpace} SPACES AVAILABLE</p>
-                    <button className="btn btn-dark border-0 background-fresh text-uppercase">Book Appointment</button>
+                    <button onClick={openModal} className="btn btn-dark border-0 background-fresh text-uppercase">Book Appointment</button>
+                    <AppointmentForm appointmentOn={booking.subject} modalIsOpen={modalIsOpen} closeModal={closeModal} ></AppointmentForm>
                 </div>
             </div>
         </div>
